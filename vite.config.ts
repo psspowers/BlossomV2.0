542974856cea12b4f3f2a9002ba78efe756dd296
Pss Powers <sam@psspowers.com>
2026-01-19 23:27:44 +0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    VitePWA({
+    mode === 'production' && VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['logo.png'],
       manifest: {
